501478a233fd3cf7d618bab2531cf091becba066
zharo <antonzarov007@gmail.com>
2023-09-23 14:06:40 +0300

    Zh_commit_111

--- src/seminar1.py
+++ src/seminar1.py
@@ -8,6 +8,7 @@ def random_matrix(n: int) -> np.array:
     """
     Make (n x n x 3) matrix with a random uniform distribution [0, 255]
     array type must be uint8
+
     :param n: matrix sizeyy
 
     :return: random n x n x 3 matrix
